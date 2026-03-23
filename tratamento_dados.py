a423f9afbdd358e28bd738d22b755f398e162239
João Valenci <valencijoao30@gmail.com>
2026-03-23 17:46:53 -0300

    Inclusão do Script com capacidade de comparar duas planilhas

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -5,36 +5,64 @@ import openpyxl
 
 def gerar_relatorio(arquivo_entrada, arquivo_saida):
 
+
     df_enviados = pd.read_excel(arquivo_entrada, sheet_name="Enviados")
     df_inseridos = pd.read_excel(arquivo_entrada, sheet_name="Inseridos")
 
-    df_enviados.columns = df_enviados.columns.str.strip()
-    df_inseridos.columns = df_inseridos.columns.str.strip()
 
-    def limpar_id(valor):
+    df_enviados.columns = (
+        df_enviados.columns
+        .str.replace('\xa0', '', regex=False)
+        .str.strip()
+    )
+
+    df_inseridos.columns = (
+        df_inseridos.columns
+        .str.replace('\xa0', '', regex=False)
+        .str.strip()
+    )
+
+
+    df_inseridos = df_inseridos.rename(columns={
+        'Licitação': 'ID',
+        'Site': 'Portal'
+    })
+
+
+    def limpar_texto(valor):
         if pd.isna(valor):
             return ""
-        
         valor = str(valor).strip()
         valor = re.sub(r'[^0-9a-zA-Z]', '', valor)
         return valor.upper()
 
-    colunas_chave_enviados = ['Portal', 'Cliente', 'Data', 'ID']
-    colunas_chave_inseridos = ['Data Licitação', 'Licitação','Site', 'Cliente']
+    colunas_chave = ['ID', 'Portal', 'Cliente']
 
-    for col in colunas_chave_enviados:
-        df_enviados[col + '_LIMPO'] = df_enviados[col].apply(limpar_id)
-
-    for col in colunas_chave_inseridos:
-        df_enviados[col + '_LIMPO'] = df_enviados[col].apply(limpar_id)
+    for col in colunas_chave:
+        df_enviados[col + '_LIMPO'] = df_enviados[col].apply(limpar_texto)
+        df_inseridos[col + '_LIMPO'] = df_inseridos[col].apply(limpar_texto)
 
 
-    df_inseridos_unico = df_inseridos[['ID_LIMPO']].drop_duplicates().copy()
+    df_enviados['CHAVE'] = (
+        df_enviados['ID_LIMPO'] + '|' +
+        df_enviados['Portal_LIMPO'] + '|' +
+        df_enviados['Cliente_LIMPO']
+    )
+
+    df_inseridos['CHAVE'] = (
+        df_inseridos['ID_LIMPO'] + '|' +
+        df_inseridos['Portal_LIMPO'] + '|' +
+        df_inseridos['Cliente_LIMPO']
+    )
+
+
+    df_inseridos_unico = df_inseridos[['CHAVE']].drop_duplicates().copy()
     df_inseridos_unico['Participou'] = 'Sim'
+
 
     df_final = df_enviados.merge(
         df_inseridos_unico,
-        on='ID_LIMPO',
+        on='CHAVE',
         how='left'
     )
 
@@ -44,7 +72,6 @@ def gerar_relatorio(arquivo_entrada, arquivo_saida):
         df_final.to_excel(writer, sheet_name="Analise Completa", index=False)
 
 
+gerar_relatorio('tecsystems_2025.xlsm', 'analise_2026.xlsx')
 
-gerar_relatorio('tecsystems_2025.xlsm','analise_2026.xlsx' )
-    
 print("Arquivo gerado com sucesso!")
